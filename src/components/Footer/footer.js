8b512af307929f438783d9d4d3eb48225d6897a6
George Ormrod <george.ormrod@desana.io>
2022-11-30 11:07:54 +0000

    feat: update footer info

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import logo from "./logo.png"
-import { IoLogoInstagram, IoLogoLinkedin } from "react-icons/io";
+import logo from './logo.png'
+import { IoLogoInstagram, IoLogoLinkedin } from 'react-icons/io'
 import * as styles from './footer.module.css'
-import { Container } from '@mui/material';
+import { Container } from '@mui/material'
 
 const Footer = () => (
   <div className={styles.root}>
     <Container maxWidth="xl">
       <div className={styles.container}>
-        <img className={styles.logo} src={logo} alt="leonard peters consulting" />
+        <img
+          className={styles.logo}
+          src={logo}
+          alt="leonard peters consulting"
+        />
         <div className={styles.desktopDiv}>
           <small>Salisbury House,</small>
           <small>London Wall,</small>
@@ -16,17 +20,35 @@ const Footer = () => (
         </div>
         <div className={styles.desktopDiv}>
           <small>info@l-p-c.com</small>
-          <small>+44 000 000 0000</small>
+          {/* <small>+44 000 000 0000</small> */}
         </div>
         <div className={styles.desktopDiv}>
           <small>Privacy</small>
         </div>
         <div>
-          <IoLogoLinkedin size={30} color="white" />
-          <IoLogoInstagram size={30} color="white" />
+          <IoLogoLinkedin
+            className={styles.socialIcon}
+            onClick={() =>
+              window.open(
+                'https://www.linkedin.com/company/leonard-peters-consulting/',
+                '_blank'
+              )
+            }
+            size={30}
+          />
+          <IoLogoInstagram
+            className={styles.socialIcon}
+            size={30}
+            onClick={() =>
+              window.open(
+                'https://www.instagram.com/leonardpetersconsulting/',
+                '_blank'
+              )
+            }
+          />
         </div>
         <div className={styles.tabletDiv}>
-          <small>© Leonard Peters Consulting 2020</small>
+          <small>© Leonard Peters Consulting 2019</small>
         </div>
       </div>
     </Container>
