27ba022d07a681fa9195aafb7d88afc9b88252f0
George Ormrod <george.ormrod@desana.io>
2022-12-01 13:37:52 +0000

    feat: add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,5 +20,23 @@ module.exports = {
         host: process.env.CONTENTFUL_HOST
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Leonard Peters Consulting",
+        short_name: "LPC",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#615D59",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    }
   ],
 };
