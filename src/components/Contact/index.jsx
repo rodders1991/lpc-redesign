f4420bfd9f031fafbe4b93c1e76885d2f5683fd5
George Ormrod <george.ormrod@desana.io>
2023-02-04 11:39:51 +0000

    fix: styling updates to contact page

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -3,7 +3,7 @@ import * as styles from './contact.module.css'
 
 import ContactSection from '../ContactSection'
 import { Container } from '@mui/material'
-import { InlineWidget } from "react-calendly"
+import { InlineWidget } from 'react-calendly'
 import { convertToBgImage } from 'gbimage-bridge'
 import BackgroundImage from 'gatsby-background-image'
 import { getImage } from 'gatsby-plugin-image'
@@ -23,18 +23,20 @@ const Contact = ({ banner }) => {
           preserveStackingContext
         >
           <div className={styles.container}>
-            <Container maxWidth="xl" className={styles.containerInside}>
-              <div>
-                <h1 className={styles.title}>Contact Us</h1>
-                <h3 className={styles.subTitle}>
-                  If you have any further questions, we suggest booking a
-                  meeting with 1 of our team of experts who would be happy to
-                  help but alternatively you can call or email us. We look
-                  forward to hearing from you!
-                </h3>
-              </div>
-              <div className={styles.calendarContainer}>
-                <InlineWidget url="https://calendly.com/georgeormrod" />
+            <Container maxWidth="xl">
+              <div className={styles.containerInside}>
+                <div>
+                  <h1 className={styles.title}>Contact Us</h1>
+                  <h3 className={styles.subTitle}>
+                    If you have any further questions, we suggest booking a
+                    meeting with 1 of our team of experts who would be happy to
+                    help but alternatively you can call or email us. We look
+                    forward to hearing from you!
+                  </h3>
+                </div>
+                <div className={styles.calendarContainer}>
+                  <InlineWidget url="https://calendly.com/georgeormrod" />
+                </div>
               </div>
             </Container>
           </div>
