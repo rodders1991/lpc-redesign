3c6f146b7682dcc2d740d37dbab63f5364417837
George Ormrod <george.ormrod@desana.io>
2023-03-27 19:52:13 +0100

    feat: add services page

---
+++ src/components/Services/index.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import * as styles from './service.module.css'
+
+import ContactSection from '../ContactSection'
+import { Container } from '@mui/material'
+import { GatsbyImage } from 'gatsby-plugin-image'
+import { Fade } from 'react-awesome-reveal'
+
+const Service = ({
+  service: { name, icon, subTitle },
+  index,
+}) => (
+  <div className={styles.service} style={{ animationDelay: `${index*0.5}s`}}>
+    <GatsbyImage
+      alt=""
+      image={icon.gatsbyImage}
+      objectFit="contain"
+      className={styles.serviceImg}
+    />
+    <div className={styles.serviceTopTextContainer}>
+      <div>
+        <h3 className={styles.serviceTitle}>{name}</h3>
+      </div>
+    </div>
+    <p className={styles.serviceAbout}>{subTitle}</p>
+  </div>
+)
+
+const Services = ({ services }) => {
+  return (
+    <>
+      <Fade>
+        <Container maxWidth="xl">
+          <h1 className={styles.title}>Services</h1>
+        </Container>
+        <div className={styles.servicesRoot}>
+          <Container maxWidth="lg">
+            <div className={styles.servicesRootContainer}>
+              {services.map((service, index) => (
+                <Service
+                  key={`${service.id}-${index}`}
+                  service={service}
+                  index={index}
+                />
+              ))}
+            </div>
+          </Container>
+        </div>
+      </Fade>
+      <ContactSection />
+    </>
+  )
+}
+
+export default Services
---
+++ src/pages/services.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { get } from 'lodash'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+import Services from '../components/Services'
+
+class TeamPage extends React.Component {
+  render() {
+    const logo = get(this, 'props.data.contentfulContactPage.logo')
+    const navigationItems = get(
+      this,
+      'props.data.contentfulContactPage.navigation.items'
+    )
+
+    const services = get(this, 'props.data.allContentfulService.nodes')
+
+    return (
+      <Layout
+        logo={logo}
+        navigation={navigationItems}
+        location={this.props.location}
+        projects={{}}
+        navigationStatic
+      >
+        <Services services={services} />
+      </Layout>
+    )
+  }
+}
+
+export default TeamPage
+
+export const pageQuery = graphql`
+  query ServicesPage {
+    contentfulContactPage {
+      navigation {
+        items {
+          name
+          link
+        }
+      }
+      logo {
+        gatsbyImage(layout: CONSTRAINED, placeholder: BLURRED, width: 300)
+      }
+    }
+    allContentfulService {
+      nodes {
+        name
+        subTitle
+        icon {
+          gatsbyImage(layout: CONSTRAINED, placeholder: BLURRED, width: 400)
+        }
+      }
+    }
+  }
+`
