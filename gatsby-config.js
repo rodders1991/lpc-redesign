7f9f5241f73f4ff649cf5a1bc357d05d3de0f09c
George Ormrod <george.ormrod@desana.io>
2023-08-31 18:29:45 +0100

    feat: setup netify packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sharp",
     "gatsby-plugin-image",
+    "gatsby-plugin-netlify",
     {
       resolve: "gatsby-source-contentful",
       options: {
